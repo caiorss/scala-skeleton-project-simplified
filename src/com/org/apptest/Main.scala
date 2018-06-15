502b7a6fc037738bb5006235d886537231426d79
CaioRS <caiorss.rodrigues@gmail.com>
2018-06-15 10:57:56 -0300

    Modify Main and add a groovy console object.

--- src/com/org/apptest/Main.scala
+++ src/com/org/apptest/Main.scala
@@ -1,15 +1,23 @@
 package com.org.apptest
 
-import javax.swing.{ JFrame, JButton }
-import java.awt.event.{ ActionListener, ActionEvent }
-
 object Main {
   
   val gui = new UserInterface()
+  
 
   def main(arr: Array[String]) = {    
     gui.setVisible(true)
+    gui.getContentPane().setBackground(java.awt.Color.WHITE)
+
+    val console = new groovy.ui.Console()
+    console.setVariable("gui", gui)
+    console.setVariable("x",  2.451)
+    console.run()
+
     println("Hello world - Scala project.")
+
+    for(i <- 1 to 10) println("i = " + i)
+
   }
 
 }
